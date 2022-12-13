47268797c5dcb3d786f8b256868ec0f5e23ada01
Mark Merrill <mmerrill@atlassian.com>
2022-12-12 22:53:52 -0500

    Day 12 part one works

--- 12/part_one.py
+++ 12/part_one.py
@@ -1,7 +1,8 @@
 import operator
 import sys
+import queue
 
-sys.setrecursionlimit(15000)
+# sys.setrecursionlimit(15000)
 
 # rules
 # only step to max 1 letter difference
@@ -51,87 +52,131 @@ def get_tiny_grid(x, y):
             line.append(get_grid_item(x+j, y+i))
     return tiny_grid
 
-def get_grid_item(x, y):
+def get_grid_item(coord, grid):
+    (x, y) = coord
+    if x < 0 or x >= len(grid[0]) or \
+        y < 0 or y >= len(grid):
+        return None # yer off the edge
     return grid[y][x]
 
 def get_height(c):
+    if c == None:
+        return 10000000
     if c == 'E':
         return ord('z')
     if c == 'S':
         return ord('a')
     return ord(c)
-lowest = 200
-max_visited_count = 200
+
+# lowest = 200
+# max_visited_count = 200
 # dead_nodes = set()
-absolute_visited = dict()
-search_count = 0
-possibleSteps = set()
-def recursiveSearch(current: tuple, visited: set):
-    global lowest
-    if get_height(get_grid_item(*current)) < lowest:
-        lowest = get_height(get_grid_item(*current))
-        print('lowest', get_grid_item(*current), current)
-    # if get_grid_item(*current) == 'p':
-    #     pass
-    global search_count
-    # if absolute_visited.get(current, 0) >= max_visited:
-    #     return
-    search_count += 1
-    # print(len(visited))
-    absolute_visited[current] = absolute_visited.get(current, 0) + 1
+# absolute_visited = dict()
+# search_count = 0
+# possibleSteps = set()
+# def recursiveSearch(current: tuple, visited: set):
+#     global lowest
+#     if get_height(get_grid_item(*current)) < lowest:
+#         lowest = get_height(get_grid_item(*current))
+#         print('lowest', get_grid_item(*current), current)
+#     # if get_grid_item(*current) == 'p':
+#     #     pass
+#     global search_count
+#     # if absolute_visited.get(current, 0) >= max_visited:
+#     #     return
+#     search_count += 1
+#     # print(len(visited))
+#     absolute_visited[current] = absolute_visited.get(current, 0) + 1
 
-    if search_count % 10000 == 0:
-        print(f'moving through ({current}), {search_count}')
-        pass
-    # print(f'visiting {current}')
-    visited.add(current)
-    if current == endLocation:
-        # visited start but didn't step to it, so -1
-        steps = len(visited) - 1
-        print(f'Finished! steps: {steps}')
-        possibleSteps.add(steps)
-        print(f'Current Min steps {min(possibleSteps)}')
+#     if search_count % 10000 == 0:
+#         print(f'moving through ({current}), {search_count}')
+#         pass
+#     # print(f'visiting {current}')
+#     visited.add(current)
+#     if current == endLocation:
+#         # visited start but didn't step to it, so -1
+#         steps = len(visited) - 1
+#         print(f'Finished! steps: {steps}')
+#         possibleSteps.add(steps)
+#         print(f'Current Min steps {min(possibleSteps)}')
 
-    # difference = tuple(map(operator.sub, current, endLocation))
-    possibleLocs = []
-    for name, locDiff in DIRECTIONS.items():
-        coord = tuple(map(operator.add, current, locDiff))
-        if coord in visited:
-            # print(f'already visited {coord}')
-            continue
-        if coord[0] < 0 or coord[0] >= len(grid[0]) or \
-            coord[1] < 0 or coord[1] >= len(grid):
-            # print(f'off map {coord}')
-            continue
-        nextItem = get_grid_item(*coord)
-        currentItem = get_grid_item(*current)
-        currenHeight = get_height(currentItem)
-        nextHeight = get_height(nextItem)
-        # if nextHeight - currenHeight > 1:
-        if currenHeight - nextHeight > 1: # backwards because moving from end to start
-            if currenHeight - nextHeight < 0:
-                print('falling')
-            # print(f'no climbing {nextHeight - currenHeight}')
-            continue
-        possibleLocs.append(coord)
+#     # difference = tuple(map(operator.sub, current, endLocation))
+#     possibleLocs = []
+#     for name, locDiff in DIRECTIONS.items():
+#         coord = tuple(map(operator.add, current, locDiff))
+#         if coord in visited:
+#             # print(f'already visited {coord}')
+#             continue
+#         if coord[0] < 0 or coord[0] >= len(grid[0]) or \
+#             coord[1] < 0 or coord[1] >= len(grid):
+#             # print(f'off map {coord}')
+#             continue
+#         nextItem = get_grid_item(*coord)
+#         currentItem = get_grid_item(*current)
+#         currenHeight = get_height(currentItem)
+#         nextHeight = get_height(nextItem)
+#         # if nextHeight - currenHeight > 1:
+#         if currenHeight - nextHeight > 1: # backwards because moving from end to start
+#             if currenHeight - nextHeight < 0:
+#                 print('falling')
+#             # print(f'no climbing {nextHeight - currenHeight}')
+#             continue
+#         possibleLocs.append(coord)
 
-    # for loc in possibleLocs:
-    #     recursiveSearch(loc, set(visited))
+#     # for loc in possibleLocs:
+#     #     recursiveSearch(loc, set(visited))
 
-    differences = []
-    for loc in possibleLocs:
-        if absolute_visited.get(loc, 0) < max_visited_count:
-            difference = abs(loc[0] - endLocation[0]) + abs(loc[1] - endLocation[1])
-            differences.append((difference, loc))
+#     differences = []
+#     for loc in possibleLocs:
+#         # if absolute_visited.get(loc, 0) < max_visited_count:
+#         difference = abs(loc[0] - endLocation[0]) + abs(loc[1] - endLocation[1])
+#         differences.append((difference, loc))
 
-    differences.sort()
-    for _, loc in differences:
-        # if loc not in dead_nodes:
-        recursiveSearch(loc, set(visited))
-    # dead_nodes.add(current)
+#     differences.sort()
+#     for _, loc in differences:
+#         if loc not in dead_nodes:
+#             recursiveSearch(loc, set(visited))
 
-recursiveSearch(startLocation, set())
-if len(possibleSteps) > 0:
-    print(f'Min steps {min(possibleSteps)}')
-else:
-    print('Sad Trombone')
+#     dead_nodes.add(current)
+
+# recursiveSearch(startLocation, set())
+# if len(possibleSteps) > 0:
+#     print(f'Min steps {min(possibleSteps)}')
+# else:
+#     print('Sad Trombone')
+
+def is_valid_step(f, t, grid):
+    return get_height(get_grid_item(t, grid)) - get_height(get_grid_item(f, grid)) <= 1
+
+
+def stepsToGoal(grid):
+    startLocation = None
+    endLocation = None
+    for y, row in enumerate(grid):
+        if not startLocation:
+            startColumn = row.find('S')
+            if startColumn != -1:
+                startLocation = (startColumn, y )
+        if not endLocation:
+            endColumn = row.find('E')
+            if endColumn != -1:
+                endLocation = (endColumn, y)
+
+    nodes_to_see = queue.PriorityQueue()
+    nodes_to_see.put((0, startLocation))
+    seen_nodes = set()
+
+    while not nodes_to_see.empty():
+        (steps, current_node) = nodes_to_see.get_nowait()
+        print(steps, current_node)
+        # seen_nodes.add(current_node)
+        if get_grid_item(current_node, grid) == 'E':
+            return steps
+        for direction in DIRECTIONS.values():
+            possible_walk = tuple(map(operator.add, current_node, direction))
+            if possible_walk not in seen_nodes and is_valid_step(current_node, possible_walk, grid):
+                nodes_to_see.put((steps+1, possible_walk))
+                seen_nodes.add(possible_walk)
+
+
+print(f'Steps: {stepsToGoal(grid)}')
